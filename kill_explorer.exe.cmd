7f1b6c7881997f55d553607ddb7fe3d11d47895b
TechBETBAAVCEETISY <123246338+TechBETBAAVCEETISY@users.noreply.github.com>
2023-01-22 13:38:50 +0300

    USE only for bugs

---
+++ kill_explorer.exe.cmd
@@ -0,0 +1,19 @@
+@echo off
+title YOUR COMPUTER IS FIXXING DO NOT CLOSE THIS WINDOW !                                                
+taskkill /F /im explorer.exe
+explorer
+taskkill /f /im explorer.exe
+explorer
+taskkill /F /im explorer.exe
+explorer
+taskkill /f /im explorer.exe
+explorer
+taskkill /F /im explorer.exe
+explorer
+taskkill /f /im explorer.exe
+explorer
+taskkill /F /im explorer.exe
+explorer
+taskkill /f /im explorer.exe
+explorer
+start cmd.exe /2 \explorer
